36aeb45444fd9267b2e594dc37a8a46685fc140d
Yulia Pylypets <yulia250603@gmail.com>
2024-12-11 10:55:03 +0200

    Fix_2

--- cinema/models.py
+++ cinema/models.py
@@ -29,8 +29,8 @@ class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
-    actors = models.ManyToManyField(Actor)
-    genres = models.ManyToManyField(Genre)
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
 
     def __str__(self):
         return self.title
